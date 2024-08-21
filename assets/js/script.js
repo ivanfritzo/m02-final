6994bade0d9e4289566504078e5cb00c0b196f03
Ivan Fritz Oyarzun <85638424+ivanfritzo@users.noreply.github.com>
2024-08-21 09:29:54 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
     $("#contactForm").on("submit",function(event) {
         event.preventDefault();
-
+/* ---------- Se toman los valores de los campos del formulario y se eliminan los espacios vacíos ---------- */
         let name = $("#name").val().trim();
         let email = $("#email").val().trim();
         let message = $("#message").val().trim();
-
+/* ---------- Verifica si alguno de los campos está vacío ---------- */
         if (name ==='' || email ==='' || message ===''){
+/* ---------- Muestra el mensaje de error ---------- */
             $("#errorAlert").removeClass("d-none");
         } else {
+/* ---------- Deja el mensaje de error oculto---------- */
             $("#errorAlert").addClass("d-none");
+/* ---------- Actualiza los elementos del resumen con los valores ingresados---------- */
             $("#summaryName").text(name);
             $("#summaryEmail").text(email);
             $("#summaryMessage").text(message);
         }
     });
+});
+
+
+document.getElementById('navbarBrand').addEventListener('click', function() {
+    location.reload();
 });
